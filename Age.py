df026abe02adffd3b417de7f59499fae34504495
Deepak <deepakdolekar0@gmail.com>
2021-09-28 14:49:40 +0530

    added

---
+++ Age.py
@@ -0,0 +1,5 @@
+s=input("Enter your name")
+age=int(input("enter your age"))
+A=100-age
+p=2021+A
+print(s,"you will turn 100 years old, in the year-",p)
---
+++ Hello.py
@@ -0,0 +1,23 @@
+print("Hello")
+i=10
+print (i)
+
+
+'''To calculate BMI'''
+
+height=1.75
+weight=65
+BMI= weight/height**2
+print(BMI)
+
+no1 = int(input("Enter no1"))
+no2= int(input ("Enter no2"))
+if(no1 < no2):
+    sum  = no1+no2
+    print(sum)
+elif(no1> 5):
+    sub = no1-no2
+    print(sub)
+else:
+    print("Hello")
+
